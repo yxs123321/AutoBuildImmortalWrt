b7bac37f7b233c8e7c32184ee47727d5ac74d309
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2025-01-01 14:02:27 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -8,30 +8,31 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
 
 # 定义所需安装的包列表
-PACKAGES=""
-PACKAGES="$PACKAGES curl"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
-PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+套餐=""
+套餐="$套餐 curl"
+套餐="$套餐 luci-i18n-diskman-zh-cn"
+套餐="$套餐 luci-i18n-firewall-zh-cn"
+套餐="$套餐 luci-i18n-filebrowser-zh-cn"
+套餐="$套餐 luci-app-argon-config"
+套餐="$套餐 luci-i18n-argon-config-zh-cn"
+套餐="$套餐 luci-i18n-opkg-zh-cn"
+套餐="$套餐 luci-i18n-ttyd-zh-cn"
+套餐="$套餐 luci-i18n-passwall-zh-cn"
+套餐="$套餐 luci-app-openclash"
+套餐="$套餐 luci-i18n-homeproxy-zh-cn"
+套餐="$套餐 openssh-sftp-server"
+套餐="$套餐 luci-i18n-dockerman-zh-cn"
+套餐="$套餐 hysteria"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
-echo "$PACKAGES"
+echo "$套餐"
 
-make image PROFILE="generic" PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$PROFILE
+make image 轮廓="generic" 套餐="$套餐" 文件="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$轮廓
 
-if [ $? -ne 0 ]; then
+如果 [ $? -讷 0 ]; 然后
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
-    exit 1
+    出口 1
 fi
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Build completed successfully."
