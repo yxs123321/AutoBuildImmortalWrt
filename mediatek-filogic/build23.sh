ab3d7881eb047862b982f76a14e18fa8c2f7dadc
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2026-03-14 09:45:50 +0800

    Update build23.sh

--- mediatek-filogic/build23.sh
+++ mediatek-filogic/build23.sh
@@ -72,6 +72,7 @@ PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn kmod-lp kmod-usb-printer"
+PACKAGES="$PACKAGES sgdisk"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
